5d5c9695e0d403e250f81eeccf09e9fb93182bec
esracck1 <esracck1@users.noreply.github.com>
2022-06-07 23:58:18 +0300

    Add project files.

---
+++ CommerceTest/Model/AuthDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommerceTest.Model
+{
+    public class AuthDbContext:IdentityDbContext
+    {
+        
+
+        public AuthDbContext()
+        {
+
+        }
+
+        public AuthDbContext(DbContextOptions<AuthDbContext> options):base(options)
+        {
+
+        }
+        
+    }
+}
---
+++ CommerceTest/ViewModels/Register.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewModels.CommerceTest
+{
+    public class Register
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password),ErrorMessage ="Şifreler uyuşmuyor. Lütfen tekrar deneyiniz.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
